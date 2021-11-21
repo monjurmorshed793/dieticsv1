931e700c7475d04e06f3827e2b6432384e92c12a
monjur <monjurmorshed793@gmail.com>
2021-11-21 21:01:07 +0600

    initial project set up

---
+++ src/main/java/com/morshed/dietics/DieticsApplication.java
@@ -0,0 +1,13 @@
+package com.morshed.dietics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morshed/dietics/DieticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morshed.dietics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
