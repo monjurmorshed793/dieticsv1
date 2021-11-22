6a154552fe2d15f630ca22e58c467e368422e26b
monjur <monjurmorshed793@gmail.com>
2021-11-23 02:26:38 +0600

    First Name and Last Name fields added in User document.

--- src/main/java/com/morshed/dietics/model/User.java
+++ src/main/java/com/morshed/dietics/model/User.java
@@ -3,6 +3,7 @@ package com.morshed.dietics.model;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
@@ -16,6 +17,10 @@ import javax.validation.constraints.NotNull;
 public class User {
   @Id
   private String id;
+  @TextIndexed(weight = 2)
+  private String firstName;
+  @TextIndexed
+  private String lastname;
   @NotNull
   @Indexed(unique = true)
   private String username;
